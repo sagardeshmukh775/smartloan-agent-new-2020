b3ff33d3e1ac59529661447cfa597129c6d9a63c
sagar <sagardeshmukh447@gmail.com>
2020-07-01 12:02:21 +0530

    updated on 01.07.2020 at 12.02

---
+++ app/src/main/java/com/smartloan/smtrick/smart_loan/view/activite/UpdateUserProfileActivity.java
@@ -0,0 +1,15 @@
+package com.smartloan.smtrick.smart_loan.view.activite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartloan.smtrick.smart_loan.R;
+
+public class UpdateUserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user_profile);
+    }
+}
