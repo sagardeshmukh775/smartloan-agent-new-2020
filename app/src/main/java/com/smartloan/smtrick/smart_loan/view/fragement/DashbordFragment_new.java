b785d0523e20d64113e94f350a89379fa261d6d5
sagar <sagardeshmukh447@gmail.com>
2020-09-11 14:23:04 +0530

    updated on 11.09.2020 at 2.22

--- app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.smartloan.smtrick.smart_loan.R;
 import com.smartloan.smtrick.smart_loan.databinding.FragmentDashboardBinding;
@@ -18,30 +19,31 @@ public class DashbordFragment_new extends Fragment {
 
     // NOTE: Removed Some unwanted Boiler Plate Codes
     private OnFragmentInteractionListener mListener;
+    private TextView txtNumberOfLeeds, txtLoanRequiredAmount, txtTotalFilesLogin, txtFilesRejected, txtFilesSanctioned, txtSanctionedAmount, txtFilesDisbursed,
+            txtTotalDisbursementAmount, txtTotalDisbursedPayoutAmt, txtTotalPayoutPaid, txtTotalBalancePayout;
 
+    public DashbordFragment_new() {
+    }
 
-    public DashbordFragment_new() {}
-
-    Spinner spinloantype,spinemptype,spinincome;
+    Spinner spinloantype, spinemptype, spinincome;
     Button emiCalcBtn;
     ProgressBar progressBar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view= inflater.inflate(R.layout.fragment_dashboard_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard_new, container, false);
 
         // NOTE : We are calling the onFragmentInteraction() declared in the MainActivity
         // ie we are sending "Fragment 1" as title parameter when fragment1 is activated
         if (mListener != null) {
             mListener.onFragmentInteraction("fragment5");
         }
-
-
 
 
         return view;
