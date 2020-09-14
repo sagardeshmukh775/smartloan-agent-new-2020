f1d93a30b61c8a60751aef577d756c99cc7ff716
sagar <sagardeshmukh447@gmail.com>
2020-09-14 10:39:51 +0530

    updated on 14.09.2020 at 10.39

--- app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,26 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.smartloan.smtrick.smart_loan.R;
+import com.smartloan.smtrick.smart_loan.callback.CallBack;
 import com.smartloan.smtrick.smart_loan.databinding.FragmentDashboardBinding;
 import com.smartloan.smtrick.smart_loan.interfaces.OnFragmentInteractionListener;
+import com.smartloan.smtrick.smart_loan.models.DashboardDataModel;
+import com.smartloan.smtrick.smart_loan.models.LeedsModel;
+import com.smartloan.smtrick.smart_loan.preferences.AppSharedPreference;
+import com.smartloan.smtrick.smart_loan.repository.LeedRepository;
+import com.smartloan.smtrick.smart_loan.repository.impl.LeedRepositoryImpl;
+import com.smartloan.smtrick.smart_loan.utilities.Utility;
+import com.smartloan.smtrick.smart_loan.view.dialog.ProgressDialogClass;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import static com.smartloan.smtrick.smart_loan.constants.Constant.LEED_DATE_FORMATE;
+import static com.smartloan.smtrick.smart_loan.constants.Constant.LOAN_TYPE_BALANCE_TRANSFER;
+import static com.smartloan.smtrick.smart_loan.constants.Constant.LOAN_TYPE_HL;
+import static com.smartloan.smtrick.smart_loan.constants.Constant.LOAN_TYPE_LAP;
+import static com.smartloan.smtrick.smart_loan.constants.Constant.YEAR_DATE_FORMAT;
 
 public class DashbordFragment_new extends Fragment {
 
@@ -23,6 +42,12 @@ public class DashbordFragment_new extends Fragment {
     private TextView txtNumberOfLeeds, txtLoanRequiredAmount, txtTotalFilesLogin, txtFilesRejected, txtFilesSanctioned, txtSanctionedAmount, txtFilesDisbursed,
             txtTotalDisbursementAmount, txtTotalDisbursedPayoutAmt, txtTotalPayoutPaid, txtTotalBalancePayout;
     private FloatingActionButton fabAddLead;
+    private ProgressDialogClass progressDialogClass;
+    private LeedRepository leedRepository;
+    AppSharedPreference appSharedPreference;
+    private long year;
+    private String yearData = "";
+    private Context context;
 
     public DashbordFragment_new() {
     }
@@ -41,9 +66,14 @@ public class DashbordFragment_new extends Fragment {
 
         // NOTE : We are calling the onFragmentInteraction() declared in the MainActivity
         // ie we are sending "Fragment 1" as title parameter when fragment1 is activated
+        context = getActivity();
         if (mListener != null) {
             mListener.onFragmentInteraction("Dash Bord");
         }
+
+        progressDialogClass = new ProgressDialogClass(getActivity());
+        leedRepository = new LeedRepositoryImpl();
+        appSharedPreference = new AppSharedPreference(getContext());
 
         txtNumberOfLeeds = view.findViewById(R.id.txtTotalLeeds);
         txtLoanRequiredAmount = view.findViewById(R.id.txtTotalLoanRequirementAmount);
@@ -58,10 +88,94 @@ public class DashbordFragment_new extends Fragment {
         txtTotalBalancePayout = view.findViewById(R.id.txtTotalbalancepayout);
 
         fabAddLead = view.findViewById(R.id.fab_add_lead);
+        Calendar now = Calendar.getInstance();
+        int currentYear = now.get(Calendar.YEAR);
+        yearData = "Year " + currentYear;
+        year = Utility.convertFormatedDateToMilliSeconds("01, " + currentYear, YEAR_DATE_FORMAT);
+
 
         addNewLeadClick();
 
         return view;
+    }
+
+    private void getDashboardData() {
+        progressDialogClass.showDialog(this.getString(R.string.loading), this.getString(R.string.PLEASE_WAIT));
+        leedRepository.readLeedsByUserIdOfYear(getActivity(), appSharedPreference.getUserId(), year, new CallBack() {
+            @Override
+            public void onSuccess(Object object) {
+                if (object != null) {
+                    ArrayList<LeedsModel> leedsModelArrayList = (ArrayList<LeedsModel>) object;
+                    if (getActivity() != null)
+                        filterDashboardData(leedsModelArrayList);
+                }else{
+                    if (getActivity() != null)
+                        filterDashboardData(new ArrayList<>());
+                }
+                if (progressDialogClass != null)
+                    progressDialogClass.dismissDialog();
+            }
+
+            @Override
+            public void onError(Object object) {
+                if (progressDialogClass != null)
+                    progressDialogClass.dismissDialog();
+                if (isAdded() && context != null)
+                    Utility.showLongMessage(context, getString(R.string.server_error));
+            }
+        });
+    }
+
+    private void filterDashboardData(ArrayList<LeedsModel> leedsModelArrayList) {
+        ArrayList<DashboardDataModel> dashboardDataModelArrayList = new ArrayList<>();
+        if (leedsModelArrayList != null) {
+            addLoanData(leedsModelArrayList);
+//            addLoginFilesData(leedsModelArrayList);
+//            addSanctionedFileData(leedsModelArrayList);
+//            addDisbursedFileData(leedsModelArrayList);
+//            addPayoutData(leedsModelArrayList);
+        }
+    }
+
+    private void addLoanData(ArrayList<LeedsModel> leedsModelArrayList ) {
+        int homeLoanFileCount = 0;
+        long homeLoanAmount = 0;
+        int loanAgainstPropertyFileCount = 0;
+        long loanAgainstPropertyAmount = 0;
+        int balanceTransferFileCount = 0;
+        long balanceTransferAmount = 0;
+        long totalAmount = 0;
+
+        if (leedsModelArrayList != null)
+            for (LeedsModel leedsModel : leedsModelArrayList) {
+                String dateString = Utility.convertMilliSecondsToFormatedDate(leedsModel.getCreatedDateTimeLong(), LEED_DATE_FORMATE);
+                Calendar c = Calendar.getInstance(TimeZone.getDefault());
+                c.setTimeInMillis(leedsModel.getCreatedDateTimeLong());
+                int mMonth = c.get(Calendar.MONTH);
+                Log.i("Dashboard fragment", "Month Formated Date:::" + dateString);
+                Log.i("Dashboard fragment", "Month Milliseconds:::" + leedsModel.getCreatedDateTimeLong());
+                Log.i("Dashboard fragment", "Month values:::" + (mMonth));
+                if (leedsModel.getLoanType().equalsIgnoreCase(LOAN_TYPE_HL)) {
+                    homeLoanFileCount++;
+                    long amount = Long.parseLong(leedsModel.getExpectedLoanAmount().replaceAll(",", ""));
+                    homeLoanAmount += amount;
+
+                } else if (leedsModel.getLoanType().equalsIgnoreCase(LOAN_TYPE_LAP)) {
+                    loanAgainstPropertyFileCount++;
+                    long amount = Long.parseLong(leedsModel.getExpectedLoanAmount().replaceAll(",", ""));
+                    loanAgainstPropertyAmount += amount;
+
+                } else if (leedsModel.getLoanType().equalsIgnoreCase(LOAN_TYPE_BALANCE_TRANSFER)) {
+                    balanceTransferFileCount++;
+                    long amount = Long.parseLong(leedsModel.getExpectedLoanAmount().replaceAll(",", ""));
+                    balanceTransferAmount += amount;
+
+                }
+                totalAmount += Double.parseDouble(leedsModel.getExpectedLoanAmount().replaceAll(",", ""));
+            }//end of for
+
+        txtNumberOfLeeds.setText(String.valueOf(leedsModelArrayList.size()));
+        txtLoanRequiredAmount.setText(String.valueOf(totalAmount));
     }
 
     private void addNewLeadClick() {
