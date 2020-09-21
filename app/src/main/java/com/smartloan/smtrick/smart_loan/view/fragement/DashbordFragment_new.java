e9905dacc62cb28860809c8393fd2be84d602979
sagar <sagardeshmukh447@gmail.com>
2020-09-21 11:33:15 +0530

    updated on 21.09.2020 at 11.33

--- app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan/view/fragement/DashbordFragment_new.java
@@ -72,7 +72,7 @@ public class DashbordFragment_new extends Fragment {
         // ie we are sending "Fragment 1" as title parameter when fragment1 is activated
         context = getActivity();
         if (mListener != null) {
-            mListener.onFragmentInteraction("DashBord");
+            mListener.onFragmentInteraction("DashaBord");
         }
 
         progressDialogClass = new ProgressDialogClass(getActivity());
